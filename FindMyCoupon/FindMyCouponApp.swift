bf14d2765c650fffbf8337bab15541eae9163c35
ryanmtran <ryanmtran922@gmail.com>
2023-07-20 23:39:07 -0400

    Initial Commit

---
+++ FindMyCoupon/FindMyCouponApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyCouponApp.swift
+//  FindMyCoupon
+//
+//  Created by Ryan Tran on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct FindMyCouponApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
